206eb53cb8d2a15a574559127bb0b00cef0c7fd7
Siddarth-R <vivek916@gmail.com>
2024-07-18 00:22:07 -0400

    Removed commented lines

--- app.py
+++ app.py
@@ -19,10 +19,7 @@ def GetUserInputs():
         area=shape.getArea()
         st.write(f"Area of Circle is {area}")
         
-        #args:list[int]=[Radius]
-        # shape:Shape= globals()[ShapeName]()
-        # AreaOfCircle=Shape.Circle.get(Radius)
-        # st.write(f"Area of Circle :blue {AreaOfCircle}]")
+       
     else:
         Length=st.number_input("Length in cm (integer)", value=0, step=1)
         Breadth=st.number_input("Breadth in cm (integer)", value=0, step=1)
@@ -30,17 +27,8 @@ def GetUserInputs():
         area = shape.getArea()
         st.write(f"Area of Rectangle is {area}")
        
-             
-        #args:list[int] = [Length,Breadth]
-        # AreaOfRectangle=Shape.Rectangle(Length,Breadth)
-        # st.write(f"Area of Circle :blue {AreaOfRectangle}]")
-    #shape:Shape= globals()[ShapeName](*args)
-    #st.write("Area of " +ShapeName+" is "+str(shape.getArea()))
- #   GetArea()
-
-#def GetArea():
-#    CalculateArea=st.button("GetArea")
        
+            
 if __name__=="__main__":
     st.set_page_config(page_title="APP TO CALCULATE AREA", 
                        page_icon=None, 
